c4443025ae60c57023105a991a88167a88cd1e4b
manuraj23 <manuraj082004@gmail.com>
2024-12-15 01:46:20 +0530

    Environment Variables

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const app= require('./10.Creating_Route_Modules');
+const dotenv=require('dotenv');
+dotenv.config({path:'./config.env'}); //This should be above app variable
 
-const port = 3000;
+const app = require('./10.Creating_Route_Modules');
+console.log(app.get('env'));
+console.log(process.env);
+
+const port = process.env.PORT ;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
